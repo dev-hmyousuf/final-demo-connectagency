b5f886932e06f0a0070d52c17ef72b01a1541b9e
Abdullah Al Nirob <122088280+ABDULLAH-YOY@users.noreply.github.com>
2024-08-21 21:57:58 +0600

    Add files via upload

---
+++ src/assets/team_pic/team.js
@@ -0,0 +1,55 @@
+const initialImages = [
+
+    ///eta holo image database. ghataghati korar dorkar nai
+    //ar akhne ki korsi sei bisoye na vableo cholbe ->abdullah
+    {
+        src: "https://scontent.fjsr11-1.fna.fbcdn.net/v/t39.30808-6/455942588_1077082164125552_7896922857167823546_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=127cfc&_nc_eui2=AeGpHintKAtKKTQLGOOUaEzLDXAMFHCBzMYNcAwUcIHMxoP7ikDbsL9u_T97Ho7OFe-w_9oWUiAAh9IzfPawmv8h&_nc_ohc=lykrK3zSAwUQ7kNvgHm29_b&_nc_ht=scontent.fjsr11-1.fna&oh=00_AYCDHAaZWTL090AO9_DKrcgEeogR3S1w4tBKkDpvpcMp2w&oe=66CA362F",
+
+
+        name: "Al Jubayer Chowdhury",
+        role: 'Digital Marketer',
+        pageLink:'/team/Jubayer-Chowdhury',
+        link:'https://www.linkedin.com/in/al-jubayer-5b44572a5?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app',
+        x:'#',
+        fb:'https://www.facebook.com/profile.php?id=100069728080074',
+    },
+    {
+        src: 'https://scontent.fjsr11-1.fna.fbcdn.net/v/t39.30808-6/456281762_1077082147458887_7770224953184231442_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=127cfc&_nc_eui2=AeF_MvB6QzoE6hPbWFZQDtamaLLGzRnGyPBossbNGcbI8OhRY735Pa8_V80pcKC3terEItBOJK1Z-YSOCbBQq0_9&_nc_ohc=o5BhDPBioxYQ7kNvgFKVnAF&_nc_ht=scontent.fjsr11-1.fna&oh=00_AYDmaBcnq1430XQrZUJ_K8cK79JTBkfhd5_MHKEVx0nikg&oe=66CA5615',
+
+
+        name: "Belal Uddin",
+        role: 'Web Developer',
+        pageLink:'/team/Belal-Uddin',
+        link:'https://www.linkedin.com/in/belal-uddin-99733229a?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app&fbclid=IwY2xjawEyvwlleHRuA2FlbQIxMAABHa49kb_QC8pFlfg2z0_KfObfR3AtsOrf04-bPdCfrLCGfOFxLOYECVVtpw_aem_mKiC57j04mWZYiGp_doa7w',
+        x:'https://www.x.com/coderbelal29',
+        fb:'https://www.facebook.com/devbelal10',
+    },
+
+    {
+        src: 'https://scontent.fjsr11-1.fna.fbcdn.net/v/t39.30808-6/456065321_1077082197458882_4012272037589738449_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=127cfc&_nc_eui2=AeEGnn5JT3kt2hxpPgRnXjsnZ-BXD79UEMdn4FcPv1QQx7B_WIJasx6QSbVw0kQWfwz8fK7YOVAhT0rNj_VJ6Ooc&_nc_ohc=R00QGkuwX2sQ7kNvgFZ2UMl&_nc_ht=scontent.fjsr11-1.fna&oh=00_AYCkPutDGM7IHxoeiS067BE52lNPoy3kkKg2koxPpLsecA&oe=66CA5182',
+
+
+        name: "Abdullah Al Nirob",
+        role: 'Web Developer',
+        pageLink:'/team/Abdullah-Al-Nirob',
+        link:'#',
+        x:'https://www.x.com/AbdullahN66467',
+        fb:'https://www.facebook.com/dev.abdullahalnirob'
+    },
+
+
+
+    {
+        src: 'https://scontent.fjsr11-1.fna.fbcdn.net/v/t39.30808-6/456050298_1077082194125549_1563550694633577787_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=127cfc&_nc_eui2=AeFrzEIsxbP3rET7V1wczQsdOlAshw_S2iE6UCyHD9LaIZIMet8oLFdo80mmCl6kMy3MMfiguUBmCn5E2TqKdpB0&_nc_ohc=fi68PiIUN-0Q7kNvgGVoY7q&_nc_ht=scontent.fjsr11-1.fna&oh=00_AYDA6vprmGAIkz4kBA8-AESEiB76V93v6fliyqyCtnTAWg&oe=66CA4FD4',
+
+
+        name: "M.H Yousuf",
+        role: 'Web Developer',
+        pageLink:'/team/M.H-Yousuf',
+        link:'https://www.linkedin.com/hmyousuf2010',
+        x:'https://x.com/hmyousuf2010',
+        fb:'https://www.facebook.com/hmyousuf3.0',
+    },
+
+]
+export default initialImages
