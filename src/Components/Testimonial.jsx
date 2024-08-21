5d35f7655a8875361fc499b729c4a9153113eff0
Abdullah Al Nirob <122088280+ABDULLAH-YOY@users.noreply.github.com>
2024-08-22 00:03:00 +0600

    Update Testimonial.jsx

--- src/Components/Testimonial.jsx
+++ src/Components/Testimonial.jsx
@@ -66,7 +66,7 @@ const Testimonial = () => {
 
   return (
     <>
-      <div className="max-w-7xl mx-auto">
+      <div className="max-w-7xl mx-auto mt-20">
         <div className="max-w-xl mb-24 mx-auto text-center">
           <span className="inline-block py-1 px-3 mb-4 text-xs font-semibold text-orange-900 bg-orange-50 rounded-full">TESTIMONIALS</span>
           <h1 className="font-heading text-4xl xs:text-6xl font-bold text-gray-900 mb-6">
