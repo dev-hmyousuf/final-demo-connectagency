f7460538e0d55e3b21e855fae1d172a8e42121e0
Abdullah Al Nirob <122088280+ABDULLAH-YOY@users.noreply.github.com>
2024-08-22 21:35:57 +0600

    Update News.jsx

--- src/Components/News.jsx
+++ src/Components/News.jsx
@@ -62,7 +62,7 @@ const News = () => {
                 className="min-w-0 outline-none flex-auto rounded-md border-0 bg-white/5 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
                 type="text"
                 name="name"
-                placeholder='Inter Your full Name'
+                placeholder='Enter Your full Name'
                 value={formData.name}
                 onChange={handleChange}
                 required
@@ -77,7 +77,7 @@ const News = () => {
                 autoComplete="email"
                 className="min-w-0 flex-auto rounded-md border-0 bg-white/5 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
                 type="email"
-                placeholder='Inter Your full Email'
+                placeholder='Enter Your full Email'
           name="email"
           value={formData.email}
           onChange={handleChange}
